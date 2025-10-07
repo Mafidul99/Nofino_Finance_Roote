7f9249fa4ec19bde458fe9b6a1547f43ffa91f29
Mafidul99 <mdonline192@gmail.com>
2025-10-07 15:59:38 +0530

    up

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,32 +1,58 @@
 const User = require("../models/user-model");
+// const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
     try {
         res.status(200).send('Welcome to Server Start Router');
-        
+
     } catch (error) {
         console.log(error);
-        res.status(400).send({msg:"page Not Found!"}); 
-        
+        res.status(400).send({
+            msg: "page Not Found!"
+        });
+
     }
 };
 
 const register = async (req, res) => {
     try {
-        const {username, email, phone, password} = req.body;
+        const {
+            username,
+            email,
+            phone,
+            password
+        } = req.body;
 
-        const userExist = await User.findOne({email});
+        const userExist = await User.findOne({
+            email
+        });
 
-        if(userExist){
-            return res.status(400).json({msg: "Email alredy Exists !..."})
+        if (userExist) {
+            return res.status(400).json({
+                msg: "Email alredy Exists !..."
+            })
         }
 
-        const userCreated = await User.create({username, email, phone, password});
+        // hash password
+        // const saltRound = 10;
+        // const hash_password = await bcrypt.hash(password, saltRound);
 
-         res.status(200).json({msg: userCreated});        
+        const userCreated = await User.create({
+            username,
+            email,
+            phone,
+            password
+        });
+
+        res.status(201).json({
+            msg: userCreated
+        });
     } catch (error) {
-        res.status(500).json("Internal Server Error !..");        
+        res.status(500).json("Internal Server Error !..");
     }
 };
 
-module.exports = {home, register};
+module.exports = {
+    home,
+    register
+};
