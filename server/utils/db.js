317ba538b01f91a04938a2b858fdcb433b91823c
Mafidul99 <mdonline192@gmail.com>
2025-10-07 14:40:02 +0530

    fst pp

---
+++ server/utils/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+
+const URI = "mongodb://127.0.0.1:27017/nofino_admin"
+// mongoose.connect(URI)
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(URI);
+        console.log("Connection Successful to Database Ok");
+        
+    } catch (error) {
+        console.error("Database Conn Failed !..");
+        process.exit(0);
+    }
+}
+
+module.exports = connectDb;
